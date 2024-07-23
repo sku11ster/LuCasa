c7c76a5d05fee841276befc6ea42e43451364a4f
sku11ster <murtazaahmed022@gmail.com>
2024-07-23 17:27:17 +0500

    Added User Model

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,17 @@ class User(AbstractUser):
         ('buyer','BUYER'),
         ('seller',"SELLER"),
     )
+    user_type = models.CharField(max_length=10,choices=USER_TYPE_CHOICES)
+    is_individual = models.BooleanField(default=True)
+    company_name = models.CharField(max_length=255,blank=True,null=True)
+    contact = models.CharField(max_length=15)
+    profile_picture =models.ImageField(upload_to='profile_pics/',default='/profile_pics/default.png')
+    profile_banner =models.ImageField(upload_to='profile_banner/',default='/profile_banner/default.png')
+    address = models.TextField(blank=True,null=True)
+    website = models.TextField(blank=True,null=True)
+    bio = models.TextField(blank=True,null=True)
+    social_links = models.JSONField(blank=True,null=True)
+
+    def __str__(self):
+        return self.username
+
