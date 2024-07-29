c9c1281445345e4219512e0d07ccd8619397046e
sku11ster <murtazaahmed022@gmail.com>
2024-07-29 22:18:10 +0500

    Added image optimization for both profile and banner image

--- accounts/signals.py
+++ accounts/signals.py
@@ -5,20 +5,28 @@ from io import BytesIO
 from django.core.files.base import ContentFile
 from .models import CustomUser
 
+def optimize_image(image, max_size):
+    image = Image.open(image)
+    image = image.convert('RGB')
+
+    image.thumbnail(max_size, Image.ANTIALIAS)
+
+    output = BytesIO()
+    image.save(output, format='JPEG', quality=85)
+    output.seek(0)
+    return ContentFile(output.read(), image.name)
+
 @receiver(post_save, sender=CustomUser)
-def optimize_profile_picture(sender, instance, **kwargs):
+def optimize_profile_images(sender, instance, **kwargs):
     if instance.profile_picture:
-        image = Image.open(instance.profile_picture)
-        image = image.convert('RGB')
-
-        image.thumbnail((800, 800), Image.ANTIALIAS)
-
-        output = BytesIO()
-        image.save(output, format='JPEG', quality=85)
-        output.seek(0)
         instance.profile_picture.save(
             instance.profile_picture.name,
-            ContentFile(output.read()),
+            optimize_image(instance.profile_picture, (300, 300)),
             save=False
         )
-        output.close()
+    if instance.profile_banner:
+        instance.profile_banner.save(
+            instance.profile_banner.name,
+            optimize_image(instance.profile_banner, (1600, 600)),
+            save=False
+        )
